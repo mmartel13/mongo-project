64d745ba50d59f4a2c4a97ec0380d6d30a551651
Megan Martel <meganmartel@Megans-MacBook-Air.local>
2022-01-26 14:15:25 -0500

    created_first_database

---
+++ index.js
@@ -0,0 +1,65 @@
+
+const mongodb = require('mongodb');
+
+const client = new mongodb.MongoClient('mongodb://localhost:27017')
+
+const connectClient = async () => {
+    await client.connect()
+    console.log('ClientConnected!')
+};
+
+const getUserCollection = () => {
+    const db = client.db('megans-db');
+    const col = db.collection('users');
+    return col;
+}
+
+const insertUser = async () => {
+    const col = getUserCollection();
+    await col.insertOne({
+        first: 'Megan',
+        last: 'Martel',
+        job: 'student',
+    });
+    console.log('User Inserted!');
+};
+
+const getUsers = async () => {
+    const col = getUserCollection();
+    const users = await col.find({}).toArray();
+    return users;
+};
+
+const getProductCollection = () => {
+    const db = client.db('megans-db');
+    const col = db.collection('products');
+    return col;
+}
+
+const insertProduct = async () => {
+    const col = getProductCollection();
+    await col.insertOne({
+        type: 'pants',
+        color: 'blue',
+        size: 'small',
+
+    });
+    console.log('Product Inserted!');
+};
+
+const getProducts = async () => {
+    const col = getProductCollection();
+    const products = await col.find({}).toArray();
+    return products;
+};
+
+connectClient()
+    .then(() => insertUser())
+    .then(() => getUsers())
+    .then((users) => console.log(users))
+    .then(() => insertProduct())
+    .then(() => getProducts())
+    .then((products) => console.log(products))
+    .then (() => client.close());
+
+
